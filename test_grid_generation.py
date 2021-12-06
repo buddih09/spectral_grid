b705f0dd330862a03d6bca6f3f2228f482ddfba0
Charitha Buddhika Heendeniya <charitha.heendeniya@supsi.ch>
2021-12-06 09:49:00 +0100

    Lugano test area coordinates added

--- test_grid_generation.py
+++ test_grid_generation.py
@@ -5,15 +5,20 @@ matplotlib.use('Qt5Agg')
 
 if __name__ == '__main__':
 
-    AROSIO = (46.043510148612334,
-              8.894011974334717,
-              46.05036097561633,
-              8.908238410949707)
+    # AROSIO = (46.043510148612334,
+    #           8.894011974334717,
+    #           46.05036097561633,
+    #           8.908238410949707)
+
+    LUGANO = (45.99958511234106,
+              8.94510269165039,
+              46.02634956657254,
+              8.977718353271484)
 
     GRAPHIC_OPTS = {
         'node_shape': 'h',
     }
 
-    mbg = MapBoxGraph(AROSIO, log_level=10)
+    mbg = MapBoxGraph(LUGANO, log_level=10)
     mbg.compute([.25, .35, .4], maxiter=100, imbalance_tol=1e-1)
     mbg.subplot(**GRAPHIC_OPTS)
